e1e56fa007848b19170a15ceb0fcdf09738d01c0
marchello <marchello.lemonchello@yandex.ru>
2023-09-03 02:52:02 +0300

    валидатор входа

---
+++ login/js/log.js
@@ -0,0 +1,19 @@
+function checkForm(el) {
+  var name = el.name.value;
+  var email = el.email.value;
+  var tel = el.tel.value;
+  var pass = el.pass.value;
+  var repass = el.repass.value;
+  var arr = [name , email , tel , pass];
+
+  if (name == "") alert("имя забыл")
+  else if (email == "") alert("почту забыл")
+  else if (tel == "") alert("телефон забыл")
+  else if (isNaN(tel) === true) alert("телефон должен состоять из цифр")
+  else if (tel.length != 11) alert("цифры должно быть 11")
+  else if (pass == "") alert("пароль забыл")
+  else if (pass != repass) alert("повтор пороля не совпал")
+  else console.log(arr);
+
+  return false;
+}
